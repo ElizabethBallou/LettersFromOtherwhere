7f4cec39b337c22c03c164edf4446930e824e413
ElizabethBallou <epb267@nyu.edu>
2020-07-08 15:13:40 -0400

    moving files into repo

---
+++ Assets/Scripts/SpreadsheetParser.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SpreadsheetParser : MonoBehaviour
+{
+    public static SpreadsheetParser instance;
+    private TextAsset promptSpreadsheet;
+    private Dictionary<int, string[]> promptDictionary;
+    private int dictionaryIDAssigner = 0;
+    [HideInInspector]
+    public int currentTextIndex = 0;
+    private bool pickNewNum = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        promptDictionary = new Dictionary<int, string[]>();
+        promptSpreadsheet = Resources.Load<TextAsset>("LFO prompts");
+        string[] rowEntries = promptSpreadsheet.text.Split('\n');
+        
+        foreach (string row in rowEntries)
+        {
+            string[] rowCells = row.Split('\t');
+            promptDictionary.Add(dictionaryIDAssigner, rowCells);
+            dictionaryIDAssigner++;
+        }
+
+        Debug.Log("The count of promptDictionary is " + promptDictionary.Count);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+        
+
+    public void RandomPromptPicker(TextMeshProUGUI prompt, TextMeshProUGUI questions)
+    {
+    
+        currentTextIndex = RandomNumGen();
+        string[] holderTextArray = promptDictionary[currentTextIndex];
+            prompt.text = holderTextArray[0];
+            questions.text = holderTextArray[1];
+        
+
+    }
+
+    public int RandomNumGen()
+    {
+
+        List<int> keys = new List<int>(promptDictionary.Keys);
+        Debug.Log("The length of keys is " + keys.Count);
+        int rand = keys[Random.Range(0, keys.Count)];
+        return rand;
+       
+    }
+
+
+    public void RemoveDictionaryKey(int index)
+    {
+        promptDictionary.Remove(index);
+    }
+}
---
+++ Assets/Scripts/buttonController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using TMPro.EditorUtilities;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonController : MonoBehaviour
+{
+    public static buttonController instance;
+    public TextMeshProUGUI titleText;
+    public Button beginButton;
+    public TextMeshProUGUI writeLetterAbout;
+    public TextMeshProUGUI promptText;
+    public TextMeshProUGUI questionText;
+    public Button acceptPromptButton;
+    public Button rejectPromptButton;
+    public TextMeshProUGUI postPickText;
+    public Button restartButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        promptText.gameObject.SetActive(false);
+        questionText.gameObject.SetActive(false);
+        acceptPromptButton.gameObject.SetActive(false);
+        rejectPromptButton.gameObject.SetActive(false);
+        writeLetterAbout.gameObject.SetActive(false);
+        postPickText.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BeginButtonPress()
+    {
+        titleText.gameObject.SetActive(false);
+        beginButton.gameObject.SetActive(false);
+        writeLetterAbout.gameObject.SetActive(true);
+        promptText.gameObject.SetActive(true);
+        questionText.gameObject.SetActive(true);
+        acceptPromptButton.gameObject.SetActive(true);
+        rejectPromptButton.gameObject.SetActive(true);
+
+        SpreadsheetParser.instance.RandomPromptPicker(promptText, questionText);
+    }
+
+    public void AcceptButtonPress()
+    {
+        Debug.Log("RUNNING ACCEPTBUTTONPRESS");
+        SpreadsheetParser.instance.RemoveDictionaryKey(SpreadsheetParser.instance.currentTextIndex);
+        promptText.gameObject.SetActive(false);
+        questionText.gameObject.SetActive(false);
+        acceptPromptButton.gameObject.SetActive(false);
+        rejectPromptButton.gameObject.SetActive(false);
+        writeLetterAbout.gameObject.SetActive(false);
+        postPickText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+
+    }
+
+    public void RejectButtonPress()
+    {
+        Debug.Log("RUNNING REJECTBUTTONPRESS");
+        SpreadsheetParser.instance.RandomPromptPicker(promptText, questionText);
+    }
+
+    public void RestartButtonPress()
+    {
+        postPickText.gameObject.SetActive(false);
+        titleText.gameObject.SetActive(true);
+        beginButton.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(false);
+
+    }
+
+}
